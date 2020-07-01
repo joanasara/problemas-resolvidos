d04c184d61894ef9662a6de764d4e9d244a4f87e
joana sara <joanasousa678@gmail.com>
2020-07-01 20:56:20 -0300

    eu vou submeter o codigo

---
+++ src/uri/Problema1004.java
@@ -0,0 +1,33 @@
+package uri;
+
+import java.util.Scanner;
+
+
+
+public class Problema1004 {
+
+	public static void main(String[] args) {
+	    
+	Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+		
+		
+		System.out.println("ENTRE COM O PRIMEIRO NUMERO");
+		x = sc.nextInt();
+		System.out.println("ENTRE COM O SEGUNDO NUMERO");
+		y = sc.nextInt();
+		
+		int prod = x * y;
+		
+		System.out.println("PROD = " + prod);
+		
+		sc.close();
+		
+		
+		
+		
+
+	}
+
+}
